c4d393423d3c7853a35ed0fc7dfb86c63b218863
Xiaohe Huang <hxhhoney@gmail.com>
2016-08-31 19:30:48 -0700

    OOP

--- Source/src/generator/stringtable/dev/GetConnection.java
+++ Source/src/generator/stringtable/dev/GetConnection.java
@@ -3,6 +3,7 @@ package generator.stringtable.dev.StringTableHUMaven;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.*;
+import java.util.HashMap;
 import java.util.Properties;
 public class GetConnection {
 	static Connection conn;
@@ -58,83 +59,6 @@ public class GetConnection {
            return app_id;
 	}
 
-    public static void generateall(String hu_type,String region,String language,String app_name) throws IOException{
-    
-    	checkConnection();
-    
-       
-        try {
-        	 //1. Get a connnection to database
-        	 //2.Create a statement
-        	 //3. Execute SQL query
-        	 //4. Process the result set
-        	createConnection(); 	
-        	String app="select APP_ID from APP where APPNAME= '"+app_name+"'";
-        	String app_id=createStm(app,"APP_ID");
-    
-            String hu="select TYPE_ID from HU where Type= '"+hu_type+"'";
-            String hu_id=createStm(hu,"Type_ID");
-
-            String rl="select RL_ID from RL where REGION='"+region+"' and LANGUAGE='"+language+"' ";
-            String rl_id=createStm(rl,"RL_ID");
-            
-            String index="select PATH_ID from PATH where APP_ID='"+app_id+"' and HU_ID='"+hu_id+"'and RL_ID='"+rl_id+"' ";
-            String index_id=createStm(index,"PATH_ID"); 
-            
-            String des="select LINK_ID from PATH where APP_ID='"+app_id+"' and HU_ID='"+hu_id+"'and RL_ID='"+rl_id+"' ";
-            String des_id=createStm(des,"LINK_ID");
-            
-            String outputfilepath="select Link from LINK where LINK_ID='"+des_id+"' ";
-            String outputfilename=createStm(outputfilepath,"Link");
-            //System.out.println(outputfilename);
-            
-            StringTableGenerator.setupoutfile(outputfilename);
-            StringTableGenerator.outfile("<strings>");
-    		System.out.println("<strings>");
-            
-    		
-            Statement stmt=conn.createStatement();
-            String path="select STR_ID,Value from Template where PATH_ID='"+index_id+"'";
-            
-            ResultSet pathid=stmt.executeQuery(path);
-            String str_id=null;
-            String value=null;
-            while(pathid.next())
-            {
-            	str_id=pathid.getString("STR_ID");
-            	value=pathid.getString("Value");
-            	
-            	String find_str_id="select ST from STR_ID where STR_ID='"+str_id+"'";
-        		String result_str_id=createStm(find_str_id,"ST");
-        		
-        		String find_str_value="select VALUE_ST from VALUE where VALUE='"+value+"'";
-        		String result_str_value=createStm(find_str_value,"VALUE_ST");
-            	
-            	if(str_id==null&&value!=null)
-    			{
-    				System.out.println("       "+result_str_value);
-    				StringTableGenerator.outfile("      "+result_str_value);
-    			}
-            	if(str_id!=null)
-            	{
-            		String outstring="     <entry id=\"$"+result_str_id+"\" "+"str="+result_str_value+"/>" ;
-                	System.out.println(outstring);
-                	StringTableGenerator.outfile(outstring);
-            	}
-            }
-            stmt.close();
-            
-            StringTableGenerator.outfile("</strings>");  
-            System.out.println("</strings>");
-            
-            //-----finish querying ---------
-            conn.close();
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
-       
-    }
-    
     public static void write(String path_id) throws SQLException, IOException{
     	Statement stmt = conn.createStatement(); 
     	String tablequery="select STR_ID,Value from Template where PATH_ID='"+path_id+"'";
@@ -167,27 +91,37 @@ public class GetConnection {
 		stmt.close();
     }
     public static void select(String entry) throws IOException{
-    	checkConnection();
+    	//checkConnection();
     	try{
-        	createConnection();
+        	//createConnection();
         	
         	Statement stmt=conn.createStatement();
         	ResultSet entryset=stmt.executeQuery(entry);
         	String path_id=null;
         	String link_id=null;
+        	String prelink="";
         	while(entryset.next())
         	{
         		path_id=entryset.getString("PATH_ID");
         		link_id=entryset.getString("LINK_ID");
         		String linkquery="select Link from LINK where LINK_ID='"+link_id+"'";
         		String link=createStm(linkquery,"Link");
-        		StringTableGenerator.setupoutfile(link);
-        		StringTableGenerator.outfile("<strings>");
-        		System.out.println("<strings>");
+        		if(prelink!=""&&!link.equals(prelink))
+        		{
+        			StringTableGenerator.outfile("</strings>");
+                	System.out.println("</strings>");
+        		}
+        		
+        		if(!link.equals(prelink)){
+        			StringTableGenerator.setupoutfile(link);
+            		StringTableGenerator.outfile("<strings>");
+            		System.out.println("<strings>");
+        		}
         		write(path_id);
-        		StringTableGenerator.outfile("</strings>");
-        		System.out.println("</strings>");
+        		prelink=link;
         	}
+        	StringTableGenerator.outfile("</strings>");
+    		System.out.println("</strings>");
         	stmt.close();
    
              //-----finish querying ---------
@@ -200,84 +134,65 @@ public class GetConnection {
     	
     }
    
-    public static void generateall(String type) throws IOException{
+    public static void generateall(String type) throws IOException, SQLException{
+    	/**
+         *  //1. Get a connnection to database
+        	 //2.Create a statement
+        	 //3. Execute SQL query
+        	 //4. Process the result set
+         */
  
     	checkConnection();
-    	try{
-        	createConnection();
-     	
-        	Statement stmt=conn.createStatement();
-        	
-        	String hu="select TYPE_ID from HU where TYPE='"+type+"'";
-        	String hu_id=createStm(hu,"TYPE_ID");
-        	
-        	String entry="select PATH_ID,LINK_ID from PATH where HU_ID='"+hu_id+"'ORDER BY LINK_ID ASC";
-        	ResultSet entryset=stmt.executeQuery(entry);  	
-        	String path_id=null;
-        	String link_id=null;
-        	while(entryset.next())
-        	{
-        		path_id=entryset.getString("PATH_ID");
-        		link_id=entryset.getString("LINK_ID");
-        		String linkquery="select Link from LINK where LINK_ID='"+link_id+"'";
-        		String link=createStm(linkquery,"Link");
-        		StringTableGenerator.setupoutfile(link);
-        		StringTableGenerator.outfile("<strings>");
-        		System.out.println("<strings>");
-        		write(path_id);
-        		StringTableGenerator.outfile("</strings>");
-        		System.out.println("</strings>");
-        	}
-        	stmt.close();
-   
-             //-----finish querying ---------
-             conn.close();
-    	}
-    	catch(SQLException e){
-    		e.printStackTrace();	
-    	}
+    	createConnection();
+    	
+    	String hu="select TYPE_ID from HU where TYPE='"+type+"'";
+    	String hu_id=createStm(hu,"TYPE_ID");
+    	
+    	String entry="select PATH_ID,LINK_ID from PATH where HU_ID='"+hu_id+"'ORDER BY LINK_ID ASC";
+    	select(entry);
+
     }
-    public static void generateall(String hu_type, String region, String language) throws IOException{
+    public static void generateall(String hu_type, String region, String language) throws IOException, SQLException{
+    	/**
+         *  //1. Get a connnection to database
+        	 //2.Create a statement
+        	 //3. Execute SQL query
+        	 //4. Process the result set
+         */
     	checkConnection();
-    	try{
-        	createConnection();
-     	
-        	Statement stmt=conn.createStatement();
-        	
-        	String hu="select TYPE_ID from HU where TYPE='"+hu_type+"'";
-        	String hu_id=createStm(hu,"TYPE_ID");
-        	
+    	createConnection();
+    	
+    	String hu="select TYPE_ID from HU where TYPE='"+hu_type+"'";
+    	String hu_id=createStm(hu,"TYPE_ID");
+    	
+        String rl="select RL_ID from RL where REGION='"+region+"' and LANGUAGE='"+language+"' ";
+        String rl_id=createStm(rl,"RL_ID");
+
+    	String entry="select PATH_ID,LINK_ID from PATH where HU_ID='"+hu_id+"'and RL_ID='"+rl_id+"' ORDER BY LINK_ID ASC";
+    	select(entry);
+    	
+    }
+    public static void generateall(String hu_type,String region,String language,String app_name) throws IOException, SQLException{
+        /**
+         *  //1. Get a connnection to database
+        	 //2.Create a statement
+        	 //3. Execute SQL query
+        	 //4. Process the result set
+         */
+    	checkConnection();
+        	createConnection(); 	
+        	String app="select APP_ID from APP where APPNAME= '"+app_name+"'";
+        	String app_id=createStm(app,"APP_ID");
+    
+            String hu="select TYPE_ID from HU where Type= '"+hu_type+"'";
+            String hu_id=createStm(hu,"Type_ID");
+
             String rl="select RL_ID from RL where REGION='"+region+"' and LANGUAGE='"+language+"' ";
             String rl_id=createStm(rl,"RL_ID");
-        	
-        	
-        	
-        	String entry="select PATH_ID,LINK_ID from PATH where HU_ID='"+hu_id+"'and RL_ID='"+rl_id+"' ORDER BY LINK_ID ASC";
-        	ResultSet entryset=stmt.executeQuery(entry);  	
-        	String path_id=null;
-        	String link_id=null;
-        	while(entryset.next())
-        	{
-        		path_id=entryset.getString("PATH_ID");
-        		link_id=entryset.getString("LINK_ID");
-        		String linkquery="select Link from LINK where LINK_ID='"+link_id+"'";
-        		String link=createStm(linkquery,"Link");
-        		StringTableGenerator.setupoutfile(link);
-        		StringTableGenerator.outfile("<strings>");
-        		System.out.println("<strings>");
-        		write(path_id);
-        		StringTableGenerator.outfile("</strings>");
-        		System.out.println("</strings>");
-        	}
-        	stmt.close();
-   
-             //-----finish querying ---------
-             conn.close();
-    	}
-    	catch(SQLException e){
-    		e.printStackTrace();
-    		
-    	}
+            
+            String entry="select PATH_ID,LINK_ID from PATH where APP_ID='"+app_id+"' and HU_ID='"+hu_id+"'and RL_ID='"+rl_id+"' ORDER BY LINK_ID ASC";
+            select(entry);
+
     }
    
 }
